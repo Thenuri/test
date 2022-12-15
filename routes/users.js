51bdd45ee66a1778f2f66521e0334e940db4dcd4
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2022-12-15 14:42:13 +0530

    Change same file

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+// hopefuly there will be a merge conflict
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
